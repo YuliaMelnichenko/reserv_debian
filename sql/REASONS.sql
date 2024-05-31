785bb033aec3569ded5ab852771626a865d2f9dc
melnichenko <julia1465835@gmail.com>
2024-05-31 15:25:10 +0700

    add sql

---
+++ sql/REASONS.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.3
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 31, 2024 at 04:16 PM
+-- Server version: 5.1.40
+-- PHP Version: 5.2.12
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `TORI`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `REASONS`
+--
+
+CREATE TABLE IF NOT EXISTS `REASONS` (
+  `ID` int(11) NOT NULL,
+  `DESCRIPTION` varchar(1024) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `REASONS`
+--
+
+INSERT INTO `REASONS` (`ID`, `DESCRIPTION`) VALUES
+(1, 'Работа на дому'),
+(2, 'Работа не в офисе'),
+(3, 'Доставка комплектующих, оборудования и т.д.'),
+(4, 'Прочее'),
+(5, 'Командировка'),
+(6, 'Отпуск'),
+(7, 'Болезнь'),
+(-1, 'Приостановка учета времени'),
+(8, 'Посещение спортзала');
