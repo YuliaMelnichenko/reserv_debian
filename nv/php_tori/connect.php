eeb4a1759ae8fbfbcfa1ccbe8726287015b385ea
melnichenko <melnichenko.j@torinsk.ru>
2024-09-26 16:08:42 +0700

    release version1

--- nv/php_tori/connect.php
+++
@@ -1,9 +0,0 @@
-<?
-$link = mysql_connect("localhost", "tori", "toriadmin")
-or die ("Could not connect to database");
-
-$db_selected = mysql_select_db('TORI', $link);
-if (!$db_selected) {
-echo "<br>Cant use intec DB: $mysql_error()<br>";
-}
-?>
