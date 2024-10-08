31e60c3da17be0b2af33a50733da2febb127b6a5
melnichenko <melnichenko.j@torinsk.ru>
2024-10-08 17:44:20 +0700

    del nv

--- nv/end.php
+++
@@ -1,9 +0,0 @@
-<?php
-// session_start();
-include_once "/var/www/tori/nv/start.php";
-$end_time = microtime();
-$end_array = explode(" ",$end_time);
-$end_time = $end_array[1] + $end_array[0];
-$time = $end_time - $_SESSION['genTimeStart'];
-printf("Страница сгенерирована за %f секунд",$time); 
-?>
--- nv/start.php
+++
@@ -1,7 +0,0 @@
-<?php
-// session_start();
-$start_time = microtime();
-$start_array = explode(" ",$start_time);
-$start_time = $start_array[1] + $start_array[0];
-$_SESSION['genTimeStart'] = $start_time;
-?>
