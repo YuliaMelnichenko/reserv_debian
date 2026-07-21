eb79b8fef74197826a57c42e95a55c865d25c0e3
Yulia Melnichenko <julia1465835@gmail.com>
2026-07-21 14:53:36 +0700

    Migrate pause counter and remove unused queries

--- ajax/get_pause_notif_count.php
+++ ajax/get_pause_notif_count.php
@@ -5,16 +5,26 @@ require_ajax_auth();
 ajax_text_headers();
 
 include_once __DIR__ . "/../funcs.php";
+require_once __DIR__ . "/../inc/notification_summary.php";
+include __DIR__ . "/../php_tori/connect.php";
 
-$notifCount = "";
+$notifCountStr = "";
 
 if ( isset( $_SESSION['ss_id'] ) )
-{ 
-  $notificationCount = 0;
-  $currentDayNotificationCount = 0;
-  $notifCount = get_pause_notif_counts( $_SESSION['ss_id'], $notificationCount, $currentDayNotificationCount );
-  if ( $currentDayNotificationCount > 0 )
-    $notifCountStr = "($currentDayNotificationCount)";    
+{
+  $counts = get_pause_notification_count(
+    $link,
+    (int)$_SESSION['ss_id'],
+    get_current_datetime_in_timezone_str(1, 0)
+  );
+
+  if ($counts === false) {
+    ajax_database_error($link, __FILE__ . ':' . __LINE__);
+    exit;
+  }
+
+  if ( $counts['current_day_count'] > 0 )
+    $notifCountStr = "(" . $counts['current_day_count'] . ")";
 }
 
 echo "<h5 class=\"biggersmall\">По приостановкам учета времени $notifCountStr</h5>";
--- tests/legacy_function_cleanup_test.php
+++ tests/legacy_function_cleanup_test.php
@@ -4,6 +4,7 @@ return function () {
     $funcs = file_get_contents(__DIR__ . '/../funcs.php');
     $removedFunctions = array(
         'get_delay_notif_counts',
+        'get_pause_notif_counts',
         'get_add_time_notif_counts',
         'get_all_delay_info_by_user',
         'get_all_add_work_info_by_user',
@@ -17,8 +18,18 @@ return function () {
         );
     }
 
-    test_assert_true(
-        strpos($funcs, 'function get_pause_notif_counts') !== false,
-        'The pause AJAX counter must remain until its caller is migrated'
+    $repository = file_get_contents(__DIR__ . '/../inc/time_journal_repository.php');
+    $removedRepositoryFunctions = array(
+        'time_journal_query_delay_statuses',
+        'time_journal_query_add_time_statuses',
+        'time_journal_query_delay_journal',
     );
+
+    foreach ($removedRepositoryFunctions as $functionName) {
+        test_assert_same(
+            0,
+            preg_match('/function\s+' . preg_quote($functionName, '/') . '\s*\(/', $repository),
+            'Unused repository helper must not be restored: ' . $functionName
+        );
+    }
 };
