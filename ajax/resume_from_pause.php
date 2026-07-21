4afa73318618e4a8c93a03b2810f3487340b128e
Yulia Melnichenko <julia1465835@gmail.com>
2026-07-21 12:46:25 +0700

    Unify pause transitions and delay journal data

--- ajax/resume_from_pause.php
+++ ajax/resume_from_pause.php
@@ -4,42 +4,33 @@ require_once __DIR__ . '/../inc/access.php';
 require_ajax_auth();
 ajax_text_headers();
 
-$userID = $_SESSION['ss_id']; 
-$ss_visiting_ID = $_SESSION['ss_visiting_ID'];
-
+$userID = (int)($_SESSION['ss_id'] ?? 0);
+$visitingID = (int)($_SESSION['ss_visiting_ID'] ?? 0);
 $pauseID = request_post_int('pauseID');
+
 require_ajax_add_time_access($pauseID);
 
 include_once __DIR__ . "/../funcs.php";
 include_once __DIR__ . "/../php_tori/connect.php";
+require_once __DIR__ . "/../inc/pause_service.php";
 
-$dtResult = get_current_datetime_in_timezone();
-
-$currentDate = $dtResult[2];
-$currentDateTime = $dtResult[1];
-
-$query = db_execute(
+$result = finish_time_pause(
   $link,
-  'UPDATE visiting SET take_pause = 0 WHERE id = ? AND user_id = ?',
-  'ii',
-  array($ss_visiting_ID, $userID)
+  $userID,
+  $visitingID,
+  $pauseID,
+  get_current_datetime_in_timezone_str(1, 0)
 );
-$merr=mysqli_error($link);
-if (!$query)
-{
-  ajax_database_error($link, __FILE__ . ':' . __LINE__);
-}
-else
-{
-$query = db_execute($link, 'UPDATE ADD_TIME SET STOP_DT = ? WHERE id = ?', 'si', array($currentDateTime, $pauseID));
 
-  if (!$query)
-  {
-    ajax_database_error($link, __FILE__ . ':' . __LINE__);
-  }
-  else
-  {
-    echo "1"; 
-  }
+if ($result === false) {
+  ajax_database_error($link, __FILE__ . ':' . __LINE__);
+  exit;
 }
+
+if ($result['status'] !== 'success') {
+  ajax_text_response($result['message']);
+  exit;
+}
+
+ajax_text_response('1');
 ?>
